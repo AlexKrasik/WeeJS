1ec23d2bc7560f4a41d219e7c503aef5f1209dfa
Oleksandr Krasikov <alexkrasik@gmail.com>
2026-08-22 16:00:09 +0300

    replace rollup with vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+import {fileURLToPath} from 'url';
+import dts from 'vite-plugin-dts';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default defineConfig({
+    plugins: [
+        dts()
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/Wee.ts'),
+            name: 'weeJS',
+            fileName: (format) => `wee.${format}.js`,
+            formats: ['es', 'iife'],
+        },
+        sourcemap: true,
+        emptyOutDir: true,
+    },
+});
