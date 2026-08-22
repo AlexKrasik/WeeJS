ae5eee4845e60072f2ba2f5992081466c5616428
Oleksandr Krasikov <alexkrasik@gmail.com>
2026-08-22 15:51:50 +0300

    Input class fixes

--- src/Input.ts
+++ src/Input.ts
@@ -1,44 +1,52 @@
-export class Input {
-    private static _keys = [];
+interface KeyState {
+    pressed: boolean;
+    down: boolean;
+    released: boolean;
+}
 
-    // TODO add controller support
+export class Input {
+    private static _keys = new Map<string, KeyState>();
+
     static init() {
         addEventListener('keydown', e => {
-            if (!this._keys[e.code]) this._defineKey(e.code);
-            if (!this._keys[e.code].down) this._keys[e.code].pressed = true;
-            this._keys[e.code].down = true;
-            this._keys[e.code].released = false;
+            const key = this._key(e.code)
+            if (!key.down) key.pressed = true;
+            key.down = true;
+            key.released = false;
         });
         addEventListener('keyup', e => {
-            if (!this._keys[e.code]) this._defineKey(e.code);
-            if (this._keys[e.code].down) this._keys[e.code].released = true;
-            this._keys[e.code].down = false;
-            this._keys[e.code].pressed = false;
+            const key = this._key(e.code);
+            if (key.down) key.released = true;
+            key.down = false;
+            key.pressed = false;
         });
     }
 
     static _clear() {
-        for (const code in this._keys) this._keys[code].pressed = this._keys[code].released = false;
-    }
-
-    private static _defineKey(code) {
-        this._keys[code] = {
-            pressed: false, down: false, released: false,
+        for (const state of Input._keys.values()) {
+            state.pressed = false;
+            state.released = false;
         }
     }
 
-    static pressed(code) {
-        if (!this._keys[code]) this._defineKey(code);
-        return this._keys[code].pressed;
+    static _key(code: string) {
+        let state = Input._keys.get(code);
+        if (!state) {
+            state = {pressed: false, down: false, released: false};
+            Input._keys.set(code, state);
+        }
+        return state;
     }
 
-    static down(code) {
-        if (!this._keys[code]) this._defineKey(code);
-        return this._keys[code].down;
+    static pressed(code: string) {
+        return this._key(code).pressed;
     }
 
-    static released(code) {
-        if (!this._keys[code]) this._defineKey(code);
-        return this._keys[code].released;
+    static down(code: string) {
+        return this._key(code).down;
+    }
+
+    static released(code: string) {
+        return this._key(code).released;
     }
 }
